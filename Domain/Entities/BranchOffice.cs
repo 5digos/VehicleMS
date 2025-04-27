3f3baf4593727a1006f7405ad952373034bec373
Nahuel Cejas <nahuelcejas89@gmail.com>
2025-04-27 18:46:16 -0300

    Uso del template

---
+++ Domain/Entities/BranchOffice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class BranchOffice
+    {
+        public int BranchOfficeId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Hours { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+        public string LocationReference { get; set; }
+
+        
+        public ICollection<Vehicle> Vehicles { get; set; }
+    }
+}
